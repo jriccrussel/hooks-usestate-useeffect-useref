b2da7eab74326552ce2d0ed8a43fc6f2457000f8
jrussel <jriccrussel@gmail.com>
2022-08-15 21:57:04 +0800

    pass prop to accordion component

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Accordion } from "./component/accordion"
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+]
+
+const App = () => {
+  return (
+    <div>
+      <Accordion items={items}/>
+    </div>
+  )
+}
+export default App
