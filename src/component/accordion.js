04741d44c93ef15b93119a16045786285a7c3163
jrussel <jriccrussel@gmail.com>
2022-08-15 22:03:35 +0800

    add styles

--- src/component/accordion.js
+++ src/component/accordion.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
 export const Accordion = ({ items }) => {
-  return (
-    <div>{items.length}</div>
-  )
+    const renderitem = items.map(item => {
+        return (
+            <React.Fragment key={item.title}>
+                <div className='title active'>
+                    <i className='dropdown icon'></i>
+                    {item.title}
+                </div>
+                <div className='content active'>
+                    <p>{item.content}</p>
+                </div>
+            </React.Fragment>
+        )
+    })
+        
+    return (
+        // <div>{items.length}</div>
+        <div className='ui styled accordion'>{renderitem}</div>
+    )
 }
