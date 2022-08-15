0e465e8a060a1a06930dd43c547bf9accd4aded7
jrussel <jriccrussel@gmail.com>
2022-08-15 22:14:27 +0800

    get index from the item to toggle

--- src/component/accordion.js
+++ src/component/accordion.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Accordion = ({ items }) => {
-    const renderitem = items.map(item => {
+    const [activeIndex, setActiveIndex] = useState(null)
+
+    // para mo toggle we need to get ang index sa item
+    const onTitleClick = (index) => {
+        // () => console.log('Title Clicked', index)
+        setActiveIndex(index)
+    }
+
+    const renderitem = items.map((item, index) => {
         return (
             <React.Fragment key={item.title}>
-                <div className='title active'>
+                <div 
+                    className='title active'
+                    // onClick={() => console.log('Title Clicked', index)}
+                    onClick={() => onTitleClick(index)}
+                >
                     <i className='dropdown icon'></i>
                     {item.title}
                 </div>
@@ -17,6 +29,9 @@ export const Accordion = ({ items }) => {
         
     return (
         // <div>{items.length}</div>
-        <div className='ui styled accordion'>{renderitem}</div>
+        <div className='ui styled accordion'>
+            {renderitem}
+            <h1>{activeIndex}</h1>
+        </div>
     )
 }
