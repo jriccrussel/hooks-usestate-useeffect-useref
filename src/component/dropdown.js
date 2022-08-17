c4d8c5330f37919668211e8a2dcd2d796cb14eec
jrussel <jriccrussel@gmail.com>
2022-08-17 13:21:28 +0800

    add show/hide function in useeffect

--- src/component/dropdown.js
+++ src/component/dropdown.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const Dropdown = ({ options, selected, onSelectedChange }) => {
+    const [open, setOpen] = useState(false)
+
+    useEffect(() => {
+        // click sa gawas select option ma automatic ma close
+        document.body.addEventListener("click", () => {
+                setOpen(false)
+            }, { capture: true }
+        )
+    }, [])
+
     const renderedOptions = options.map((option) => {
+        // sa if statement kung unsa ang current na selected item dili cya makita sa additional option 
+        // e leave ang current selection na wla kapariha
+        if(option.value === selected.value){
+            return null
+        }
+
         return (
             <div 
                 key={option.value} 
@@ -17,10 +33,14 @@ export const Dropdown = ({ options, selected, onSelectedChange }) => {
         <div className="ui form">
             <div className="field">
                 <label className="label">Select a Color</label>
-                <div className="ui selection dropdown visible active">
+                <div 
+                    // show/hide ang select option
+                    onClick={() => setOpen(!open)}
+                    className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+                >
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
-                    <div className="menu visible transition">{renderedOptions}</div>
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>{renderedOptions}</div>
                 </div>
             </div>
         </div>
