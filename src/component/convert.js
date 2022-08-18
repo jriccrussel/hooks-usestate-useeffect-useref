8327c9fe37a5c3876bad6803eba7337d3f647a72
jrussel <jriccrussel@gmail.com>
2022-08-18 16:03:12 +0800

    add debounce and finishing translate component

--- src/component/convert.js
+++ src/component/convert.js
@@ -3,27 +3,40 @@ import React, { useEffect, useState } from 'react'
 
 export const Convert = ({ language, text }) => {
     const [translated, setTranslated] = useState('')
+    const [debounceTxt, setDebounceTxt] = useState(text)
 
     useEffect(() => {
-    //   console.log('New Langauge or Text')
-    const doTranslation = async () => {
-        const { data } = await axios.post(
-            'https://translation.googleapis.com/language/translate/v2',
-            //  ang ka duha na argument sa axios add information sa body 
-            {},
-            {
-                params: {
-                    q: text,
-                    target: language.value,
-                    key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
-                },
-            }
-        )
-        setTranslated(data.data.translations[0].translatedText)
-    }
+        const timerId = setTimeout(() => {
+            setDebounceTxt(text);
+        }, 500);
+    
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [text])
+    
 
-    doTranslation()
-    }, [language, text])
+    useEffect(() => {
+        //   console.log('New Langauge or Text')
+        const doTranslation = async () => {
+            const { data } = await axios.post(
+                'https://translation.googleapis.com/language/translate/v2',
+                //  ang ka duha na argument sa axios add information sa body 
+                {},
+                {
+                    params: {
+                        // q: text,
+                        q: debounceTxt,
+                        target: language.value,
+                        key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+                    },
+                }
+            )
+            setTranslated(data.data.translations[0].translatedText)
+        }
+
+        doTranslation()
+    }, [language, debounceTxt])
     
 
     return (
